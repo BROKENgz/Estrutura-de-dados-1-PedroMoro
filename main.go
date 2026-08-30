b2018b1d1b5690d29f3d6506f9852045798b1d6f
Pedro Moro <pegrumo@gmail.com>
2026-08-30 03:08:04 -0300

    Implementa adicionar posição

---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+type no struct {
+	valor   int
+	proximo *no
+}
+
+type lista struct {
+	inicio *no
+}
+
+// Exercício 2
+func (l *lista) adicionarInicio(valor int) {
+	novo := &no{valor: valor}
+	novo.proximo = l.inicio
+	l.inicio = novo
+}
+
+func (l *lista) adicionarFim(valor int) {
+	novo := &no{valor: valor}
+
+	if l.inicio == nil {
+		l.inicio = novo
+		return
+	}
+
+	atual := l.inicio
+	for atual.proximo != nil {
+		atual = atual.proximo
+	}
+	atual.proximo = novo
+}
+
+// Exercício 3
+func (l *lista) adicionarPosicao(valor, posicao int) bool {
+	if posicao < 0 {
+		return false
+	}
+
+	if posicao == 0 {
+		l.adicionarInicio(valor)
+		return true
+	}
+
+	anterior := l.inicio
+	for i := 0; i < posicao-1 && anterior != nil; i++ {
+		anterior = anterior.proximo
+	}
+
+	if anterior == nil {
+		return false
+	}
+
+	novo := &no{valor: valor}
+	novo.proximo = anterior.proximo
+	anterior.proximo = novo
+	return true
+}
